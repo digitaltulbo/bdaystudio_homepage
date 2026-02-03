e682e935c5c503ad7752c4fdd82f72c4b0218a13
jinchoi <jinito@jinchoiui-Macmini.local>
2026-02-03 16:49:21 +0900

    Install and integrate Vercel Speed Insights

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./globals.css";
 
 export const metadata = {
@@ -41,6 +42,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
